860e57a70766811bb44087cc0ea2aa1511f39530
D3VIDE <errandojose03@gmail.com>
2025-06-08 18:29:01 +0700

    Perbaiki Format Folder

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+  <script src="https://cdn.tailwindcss.com"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+  <title>Main UI</title>
+</head>
+<body>
+  @include('layouts.navbar.user');
+  <div class="container">
+
+  </div>
+</body>
+</html>
